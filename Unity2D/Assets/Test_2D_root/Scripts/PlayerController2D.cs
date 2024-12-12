69b54487f74d39d880fee024b767ad93940ae1a4
antonimf17 <antonimoll.20novembre@gmail.com>
2024-12-12 13:12:55 +0100

    Correr
    Correr

--- Unity2D/Assets/Test_2D_root/Scripts/PlayerController2D.cs
+++ Unity2D/Assets/Test_2D_root/Scripts/PlayerController2D.cs
@@ -9,24 +9,28 @@ public class PlayerController2D : MonoBehaviour
        //Referencias Generales  //SeriazlizedField hace que sea privado pero se vea en el inspector
            [SerializeField] Rigidbody2D playerRB; //Referencia al rigidbody del player
     [SerializeField] PlayerInput playerInput; //Referencia al gestor del input del jugador
-
+    [SerializeField] Animator PlayerAnim; //Referencia para gestionar las transciones de animación
 
 
     [Header("Movement Parameters")]
     private Vector2 moveInput; //Almacén del input del player
     public float Speed;
-    
+    [SerializeField] bool isFacingRight;
 
     [Header("Jump parameters")]
     public float jumpForce;
     [SerializeField] bool isGrounded;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         //autoreferenciar componentes: nombre de variable = GetComponent()
         playerRB = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
-
+        PlayerAnim = GetComponent<Animator>();
+        isFacingRight = true;
        
 
     }
@@ -34,7 +38,21 @@ public class PlayerController2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        OnAnimation();
+        if (moveInput.x > 0 )
+        {
+            if (!isFacingRight)
+            {
+                Flip();
+            }
+        }
+        if(moveInput.x < 0)
+        {
+            if (isFacingRight)
+            {
+                Flip();
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -48,7 +66,22 @@ public class PlayerController2D : MonoBehaviour
      playerRB.velocity = new Vector3(moveInput.x * Speed, playerRB.velocity.y, 0);
       
     }
+
+    void Flip()
+    {
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
+        isFacingRight = !isFacingRight; //nombre de bool = a !nombre de bool (cambio al estado contrario)
+    }
   
+    void OnAnimation()
+    {
+        //Conector de valores generales con parámetros de cambio de animación
+        PlayerAnim.SetBool("IsJumping", !isGrounded);
+        if (moveInput.x > 0 || moveInput.x < 0) PlayerAnim.SetBool("IsRunning", true);
+        else PlayerAnim.SetBool("IsRunning", false);
+    }
 
     #region Input Events
     //Para Crear un evento
